5982866a08e4322c708d995a681841920a747b00
Yiming Wang <ywan0259@student.monash.edu>
2021-08-04 17:26:39 +1000

    Add project files.

---
+++ FIT5042-MyCSS/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace FIT5042_MyCSS
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ FIT5042-MyCSS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FIT5042_MyCSS.MvcApplication" Language="C#" %>
